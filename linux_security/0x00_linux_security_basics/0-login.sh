4dff8dfa350da092434c5e681acf6e65fb696227
Mexfi <cenab.1994@gmail.com>
2025-12-22 17:07:11 +0000

    sdsd

--- linux_security/0x00_linux_security_basics/0-login.sh
+++ linux_security/0x00_linux_security_basics/0-login.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
-if [ "$(id -u)" -ne 0 ]; then
-    echo "This script must be run as root or with sudo."
-    exit 1
+
+# Check if the script is being run as root/sudo
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root or with sudo." 
+   exit 1
 fi
-last -n 5
+
+# Use the 'last' command to fetch the 5 most recent sessions
+# -n 5 limits the output to 5 lines
+# -F ensures full date and time format is displayed
+last -n 5 -F
