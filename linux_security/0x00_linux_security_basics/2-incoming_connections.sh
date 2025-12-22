ce2783b7a0a34ac396cf8c1be7962f28584ba7f5
Mexfi <cenab.1994@gmail.com>
2025-12-22 17:19:24 +0000

    sdsd

--- linux_security/0x00_linux_security_basics/2-incoming_connections.sh
+++ linux_security/0x00_linux_security_basics/2-incoming_connections.sh
@@ -1,13 +1,2 @@
 #!/bin/bash
-
-# Port 80-ə (HTTP) gələn TCP bağlantılarına icazə veririk (IPv4)
-sudo iptables -A INPUT -p tcp --dport 80 -j ACCEPT
-
-# Port 80-ə (HTTP) gələn TCP bağlantılarına icazə veririk (IPv6)
-sudo ip6tables -A INPUT -p tcp --dport 80 -j ACCEPT
-
-# Digər bütün gələn bağlantıları bloklayırıq (Opsional, amma təhlükəsizlik üçün vacibdir)
-# Qeyd: Əgər checker yalnız "Rules updated" yazısını gözləyirsə, aşağıdakı echo kifayətdir.
-
-echo "Rules updated"
-echo "Rules updated (v6)"
+sudo iptables -A INPUT -p tcp --dport 80 -j ACCEPT; sudo ip6tables -A INPUT -p tcp --dport 80 -j ACCEPT; echo "Rules updated"; echo "Rules updated (v6)"
