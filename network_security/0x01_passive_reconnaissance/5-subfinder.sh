78e4ac1ad10008eb60cda08df8506116dca178bf
Mexfi <cenab.1994@gmail.com>
2025-12-22 19:46:56 +0000

    ddfdf

--- network_security/0x01_passive_reconnaissance/5-subfinder.sh
+++ network_security/0x01_passive_reconnaissance/5-subfinder.sh
@@ -1,11 +1,2 @@
 #!/bin/bash
-
-subfinder -d $1 -silent | tee /tmp/subs.txt
-
-> "$1.txt"
-while read sub; do
-  ip=$(dig +short $sub | head -n 1)
-  if [ -n "$ip" ]; then
-    echo "$sub,$ip" >> "$1.txt"
-  fi
-done < /tmp/subs.txt
+subfinder -d $1 -silent | tee >(awk '{ "dig +short "$1 | getline ip; if (ip!="") print $1","ip }' > $1.txt)
