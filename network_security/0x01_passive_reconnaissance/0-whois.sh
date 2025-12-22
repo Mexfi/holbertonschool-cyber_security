5a6ab9722455cae29f666f5f01cefcae971e63b3
Mexfi <cenab.1994@gmail.com>
2025-12-22 17:57:47 +0000

    sdsd

--- network_security/0x01_passive_reconnaissance/0-whois.sh
+++ network_security/0x01_passive_reconnaissance/0-whois.sh
@@ -2,15 +2,14 @@
 # Usage: ./0-whois.sh domain.com
 
 domain="$1"
-output="${domain}.csv"
 
 whois "$domain" | awk -F: '
 BEGIN { OFS="," }
-# Exactly 2 lines: Registrant Name and Registrant Organization
+# Tam olarak 2 satır: Registrant Name ve Registrant Organization
 /^Registrant Name[[:space:]]*:/ {
   v=$2; sub(/^ /,"",v); print "Registrant Name",v
 }
 /^Registrant Organization[[:space:]]*:/ {
   v=$2; sub(/^ /,"",v); print "Registrant Organization",v
 }
-' > "$output"
+' > "${domain}.csv"
