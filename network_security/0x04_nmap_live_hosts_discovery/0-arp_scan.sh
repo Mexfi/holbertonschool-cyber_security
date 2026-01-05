ccc3c2fd1f0336d98fe9c827ffa4522844860db9
Mexfi <cenab.1994@gmail.com>
2026-01-05 10:57:49 +0000

    sdsd

--- network_security/0x04_nmap_live_hosts_discovery/0-arp_scan.sh
+++ network_security/0x04_nmap_live_hosts_discovery/0-arp_scan.sh
@@ -1,19 +1,2 @@
 #!/bin/bash
-
-# Check if script is run as root
-if [ "$EUID" -ne 0 ]; then
-    echo "Please run as root or with sudo"
-    exit 1
-fi
-
-# Check if a subnet argument is provided
-if [ -z "$1" ]; then
-    echo "Usage: $0 <subnet>"
-    echo "Example: $0 192.168.65.0/24"
-    exit 1
-fi
-
-SUBNET=$1
-
-# ARP scan using nmap (host discovery only, no port scan)
-nmap -sn --arp-scan "$SUBNET"
+sudo nmap -sn --arp-scan "$1"
