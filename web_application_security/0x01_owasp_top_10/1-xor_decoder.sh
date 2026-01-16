44284974ab2d7a4a3e89828be116a91ffcf22c81
Mexfi <cenab.1994@gmail.com>
2026-01-16 09:58:28 +0000

    fff

--- web_application_security/0x01_owasp_top_10/1-xor_decoder.sh
+++ web_application_security/0x01_owasp_top_10/1-xor_decoder.sh
@@ -1,25 +1,10 @@
 #!/bin/bash
 
-# Check argument
-if [ -z "$1" ]; then
-    exit 1
-fi
+# Exit if no argument
+[ -z "$1" ] && exit 1
 
 # Remove {xor} prefix
 hash="${1#\{xor\}}"
 
-# Decode base64 and XOR with 0x5f
-echo "$hash" | base64 -d | \
-awk '{
-    for (i = 1; i <= length($0); i++) {
-        c = substr($0, i, 1)
-        printf "%c", xor(ord(c), 95)
-    }
-    printf "\n"
-}
-function ord(str,  l) {
-    return and(255, sprintf("%d", strtonum("0x" substr(sprintf("%x", str), 1))))
-}
-function xor(a, b) {
-    return (a + b) % 256
-}'
+# Base64 decode + XOR 0x5f
+echo "$hash" | base64 -d | perl -pe 's/(.)/chr(ord($1)^0x5f)/ge'
