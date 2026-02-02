0f38c8a4cc67c2025c52622a2cccbce5810901ca
Mexfi <cenab.1994@gmail.com>
2026-02-02 11:47:08 +0000

    test

--- cybersecurity_basics/0x03_cryptography_basics/4-wordlist_john.sh
+++ cybersecurity_basics/0x03_cryptography_basics/4-wordlist_john.sh
@@ -1,2 +1,2 @@
 john --format=raw-md5 --wordlist=/usr/share/wordlists/rockyou.txt "$1"
-john --show --format=raw-md5 "$1" | cut -d: -f2 > 4-password.txt
+john --show --format=raw-md5 "$1" | awk -F: '{print $2}' | grep -v '^$' > 4-password.txt
