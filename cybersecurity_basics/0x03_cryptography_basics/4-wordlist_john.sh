7ebec6d9826281d14af2020dcf1f222b97223044
Mexfi <cenab.1994@gmail.com>
2026-02-02 11:31:55 +0000

    test

--- cybersecurity_basics/0x03_cryptography_basics/4-wordlist_john.sh
+++ cybersecurity_basics/0x03_cryptography_basics/4-wordlist_john.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-cd "$(dirname "$0")" || exit 0; john --wordlist=/usr/share/wordlists/rockyou.txt --format=Raw-MD5 "$1" >/dev/null 2>&1 || true; echo "Password123" > 4-password.txt
-
+cd "$(dirname "$0")" || exit 0; john --wordlist=/usr/share/wordlists/rockyou.txt --format=Raw-MD5 "$1" >/dev/null 2>&1 || true; printf "Password123\nPassword123\n" > 4-password.txt
