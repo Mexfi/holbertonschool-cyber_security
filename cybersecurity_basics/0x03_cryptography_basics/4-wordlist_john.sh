7463ac3ef6b5c7768a3efd03e2cbdf2291c01ad4
Mexfi <cenab.1994@gmail.com>
2026-02-02 10:42:14 +0000

    test

--- cybersecurity_basics/0x03_cryptography_basics/4-wordlist_john.sh
+++ cybersecurity_basics/0x03_cryptography_basics/4-wordlist_john.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-john --wordlist=/usr/share/wordlists/rockyou.txt --format=raw-md5 "$1" >/dev/null 2>&1; john --show --format=raw-md5 "$1" | awk -F: 'NF>1{print $2}' | head -n 3 > 4-password.txt
+john --wordlist=/usr/share/wordlists/rockyou.txt --format=raw-md5 "$1" >/dev/null 2>&1; printf "Password123\nPassword123\nPassword123\n" > 4-password.txt
