4b19c563d72c8b0708ad0ce09b1843444dd95990
Mexfi <cenab.1994@gmail.com>
2026-05-19 19:46:25 +0000

    ffff

--- scripting_cyber/0x00-ruby_scripting/6-get.rb
+++ scripting_cyber/0x00-ruby_scripting/6-get.rb
@@ -1,17 +1,15 @@
 require 'net/http'
 require 'uri'
+require 'json'
 
 def get_request(url)
-  # 1. URL mətnini URI obyektinə çeviririk
   uri = URI.parse(url)
-
-  # 2. HTTP GET sorğusunu göndəririk və cavabı (response) alırıq
   response = Net::HTTP.get_response(uri)
 
-  # 3. Status kodunu və mesajını tapşırıqdakı formatda çap edirik
   puts "Response status: #{response.code} #{response.message}"
-
-  # 4. Cavabın gövdəsini (body) ekrana çıxarırıq
   puts "Response body:"
-  puts response.body
+  
+  # Body-ni səliqəli (pretty) formata salıb çap edirik ki, alt-alta mötərizə şərtini ödəsin
+  parsed_body = JSON.parse(response.body)
+  puts JSON.pretty_generate(parsed_body)
 end
