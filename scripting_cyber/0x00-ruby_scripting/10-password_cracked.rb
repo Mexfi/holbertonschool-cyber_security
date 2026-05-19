b8a5047627a253d83ac1b94f641ec242d837e38e
Mexfi <cenab.1994@gmail.com>
2026-05-19 19:54:50 +0000

    dfdfdfdf

--- scripting_cyber/0x00-ruby_scripting/10-password_cracked.rb
+++ scripting_cyber/0x00-ruby_scripting/10-password_cracked.rb
@@ -1,37 +1,32 @@
 require 'digest'
 
-# 1. Arqumentlərin sayını yoxlayırıq
 if ARGV.length != 2
   puts "Usage: 10-password_cracked.rb HASHED_PASSWORD DICTIONARY_FILE"
   exit
 end
 
-target_hash = ARGV[0].downcase.strip
-dictionary_path = ARGV[1]
+hashed_password = ARGV[0].downcase.strip
+dictionary_file = ARGV[1]
 
-# Lüğət faylının mövcudluğunu yoxlayırıq
-unless File.exist?(dictionary_path)
+unless File.exist?(dictionary_file)
   puts "Error: Dictionary file not found."
   exit
 end
 
 password_found = nil
 
-# 2. Lüğəti sətir-sətir oxuyub heşləri müqayisə edirik
-File.foreach(dictionary_path) do |line|
-  word = line.strip # Sətir sonundakı boşluq və yeni sətir (\n) simvollarını təmizləyirik
+File.foreach(dictionary_file) do |line|
+  word = line.strip
   next if word.empty?
 
-  # Sözün SHA-256 heşini hesablayırıq
   current_hash = Digest::SHA256.hexdigest(word)
 
-  if current_hash == target_hash
+  if current_hash == hashed_password
     password_found = word
     break
   end
 end
 
-# 3. Nəticəni ekrana çıxarırıq
 if password_found
   puts "Password found: #{password_found}"
 else
