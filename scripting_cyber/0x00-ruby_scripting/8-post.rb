25c368ec17b8d98dd0dcfcb5dd6e43651ae6795c
Mexfi <cenab.1994@gmail.com>
2026-05-19 19:51:28 +0000

    ffgf

--- scripting_cyber/0x00-ruby_scripting/8-post.rb
+++ scripting_cyber/0x00-ruby_scripting/8-post.rb
@@ -5,24 +5,24 @@ require 'json'
 def post_request(url, body_params = {})
   uri = URI.parse(url)
 
-  # 1. POST sorğusu obyektini yaradırıq
   request = Net::HTTP::Post.new(uri)
-  
-  # 2. Başlıqları təyin edirik (Məlumatın JSON olduğunu bildirmək üçün)
   request['Content-Type'] = 'application/json'
-  
-  # 3. Parametrləri JSON mətninə çevirib sorğunun daxilinə (body) qoyuruq
   request.body = body_params.to_json
 
-  # 4. HTTP bağlantısını qurub sorğunu göndəririk
   response = Net::HTTP.start(uri.hostname, uri.port, use_ssl: uri.scheme == 'https') do |http|
     http.request(request)
   end
 
-  # 5. Nəticələri çap edirik
   puts "Response status: #{response.code} #{response.message}"
   puts "Response body:"
 
   parsed_body = JSON.parse(response.body)
-  puts JSON.pretty_generate(parsed_body)
+
+  # Əgər gələn JSON boşdursa (yəni {}), yan-yana bitişik çap edirik
+  if parsed_body.empty?
+    puts "{}"
+  else
+    # Əgər doludursa, önbəön verilən o gözəl nizamı (pretty) qoruyuruq
+    puts JSON.pretty_generate(parsed_body)
+  end
 end
